621f3790dd5b2263267c1cbd6eaa2e8edbb91296
nnhoang215 <nnhoang215@gmail.com>
2024-08-07 17:09:06 +1000

    Home & Login UI

---
+++ my-app/src/app/(features)/generate-flashcards/page.tsx
@@ -0,0 +1,8 @@
+export default function GenFlashcardsHome() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      Files taken: pdf, video, audio, image, text. <br />
+      Description: put in instructions on how I could make flashcards that are best suited for you.
+    </main>
+  );
+}
---
+++ my-app/src/app/auth/page.tsx
@@ -0,0 +1,80 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import mainLogo from "../../../assets/googly_study_buddy_logo.png";
+import Link from 'next/link';
+
+export default function AuthLoginPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+
+      <Link href="/" >
+        <Image width="150" alt="logo" height="150" src={mainLogo} />
+      </Link>
+      <span className="p-2"></span>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" defaultValue="Austin Nguyen" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" defaultValue="@FreakyAss" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Login</Button> {/* TODO */}
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle>Sign Up</CardTitle>
+              <CardDescription>
+                Sign up here, it's free. Selling ur info btw 😈
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="current">New password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">Confirm password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Sign Up (I agree to sell my info)</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </main>
+  );
+}
